721d007ad60f67712b38f473f60c5ae069f6bb55
troy <troywebb95@gmail.com>
2022-04-06 15:23:12 +0100

    test request

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/hello", (req, res) => {
-  "Hello World";
+  res.json("Hello World");
 });
 
 app.post("/refresh", (req, res) => {
